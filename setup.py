fc489f9a765f5670656cb2457a98d49c2fcce219
Ryan Northey <ryan@space23.net>
2013-08-16 12:18:40 +0100

    setup.py]

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+
+setup(
+    name='django-grow',
+    version='0.0.1',
+    description='Grid row layout for Django',
+    author='Ryan Northey',
+    author_email='ryan@space23.net',
+    long_description=open('README.md', 'r').read(),
+    packages=[
+        'django_grow',
+        'django_grow.models'],
+    package_data={
+        'django_grow': [
+            'static/django_grow/css/*']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
